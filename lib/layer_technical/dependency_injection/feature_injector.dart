473c63a6b63be1504d57df60a68797d330412379
Nicolas <nicolas.mouginot.nadal@gmail.com>
2026-05-29 12:55:23 +0200

    💡delete a useless comment

--- lib/layer_technical/dependency_injection/feature_injector.dart
+++ lib/layer_technical/dependency_injection/feature_injector.dart
@@ -5,25 +5,3 @@ abstract interface class FeatureInjector {
   void registerUseCases(GetIt locator) {}
   void registerCubits(GetIt locator) {}
 }
-
-// Example !
-// class ReservationInjector implements FeatureInjector {
-//   @override
-//   void registerRepositories(GetIt locator) {
-//     locator.registerLazySingleton<ReservationRepository>(
-//       () => ReservationRepositoryImpl(locator()),
-//     );
-//   }
-
-//   @override
-//   void registerUseCases(GetIt locator) {
-//     locator.registerLazySingleton(() => CreateReservationUseCase(locator()));
-//     locator.registerLazySingleton(() => DeleteReservationUseCase(locator()));
-//   }
-
-//   @override
-//   void registerCubits(GetIt locator) {
-//     locator.registerFactory(() => ReservationCubit(locator(), locator()));
-//   }
-
-// }
